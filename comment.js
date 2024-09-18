b5d9f2c81595e707168882cf43d5952e34110262
Mohamed El Demery <93947139+university27@users.noreply.github.com>
2024-09-18 11:01:03 +0000

    Copilot Third Commit

---
+++ comment.js
@@ -0,0 +1,28 @@
+//create web server
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+
+var server = http.createServer(function (request, response) {
+    var path = url.parse(request.url).pathname;
+    switch (path) {
+        case '/':
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write('<h1>Welcome to the home page</h1>');
+            response.end();
+            break;
+        case '/comment':
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write('<h1>Welcome to the comment page</h1>');
+            response.end();
+            break;
+        default:
+            response.writeHead(404);
+            response.write("opps this doesn't exist - 404");
+            response.end();
+            break;
+    }
+});
+
+server.listen(8001);
+console.log('Server running at http://
